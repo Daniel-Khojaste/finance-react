788f0c7aeed82bef74999712307db3e17f95d553
Danial-Khojaste <daniel.khojaste@gmail.com>
2025-03-21 15:58:18 +0330

    NewsLetter section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
+import NewsLetter from "./Components/NewsLetter";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Analytics/>
+      <Analytics />
+      <NewsLetter />
     </div>
   );
 }
